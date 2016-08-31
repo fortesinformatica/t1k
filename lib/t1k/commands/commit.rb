fed5dd6aa97a72211ca7d8dd6e2af3a62e13f1b5
Heron Medeiros <heron.medeiros@gmail.com>
2016-08-31 16:36:09 -0300

    Now it refers to the issue when it isn't a close commit

--- lib/t1k/commands/commit.rb
+++ lib/t1k/commands/commit.rb
@@ -4,23 +4,20 @@ module T1k
 
       def self.run(message, close, add, no_message)
         message ||= ""
-        skip_ci = ''
         text_close = ''
 
         raise "Message can't be blank. Use --no-message to ignore this rule." if message.empty? && !no_message
         branch = `git branch | grep '*' | awk '{print $2}'`
 
-        text_close = close ? 'close ' : ''
+        text_close = close ? 'close ' : 'ref'
 
         if close
           text_close = 'close '
-        else
-          skip_ci    = '[ci skip]'
         end
 
         text_add = add ? 'a' : ''
 
-        system "git commit -#{text_add}m '[#{text_close}##{branch.strip}] #{message} #{skip_ci}' "
+        system "git commit -#{text_add}m '[#{text_close}##{branch.strip}] #{message}'"
       end
     end
   end
---
+++ spec/t1k/commands/commit_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe T1k::Commands::Commit do
+  describe '#self.run' do
+    let (:msg)        { "teste" }
+    let (:close)      { false   }
+    let (:add)        { false   }
+    let (:no_message) { true    }
+
+    it "should raise an exception" do
+      message    = ""
+      no_message =  false
+
+      expect { T1k::Commands::Commit.run( message, close, add, no_message) }.to raise_error("Message can't be blank. Use --no-message to ignore this rule.")
+    end
+
+    it "it have to skip circle ci when is not a close commit" do
+      close = false
+      expect_any_instance_of(Kernel).to receive(:system).with("git commit -m '[ref#branch_name] teste'")
+
+      T1k::Commands::Commit.run( msg, close, add, no_message)
+    end
+
+    it "have to close the issue when close=true is passed" do
+      close = true
+      expect_any_instance_of(Kernel).to receive(:system).with("git commit -m '[close #branch_name] teste'")
+
+      T1k::Commands::Commit.run( msg, close, add, no_message)
+    end
+  end
+
+end
