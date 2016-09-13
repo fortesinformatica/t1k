7c86135ef060a509307a3a1956c74b0474a68d28
Heron Medeiros <heron.medeiros@gmail.com>
2016-09-13 10:38:55 -0300

    added forgotten end

--- lib/t1k/commands/ship.rb
+++ lib/t1k/commands/ship.rb
@@ -1,32 +1,33 @@
-module T1k
-  module Commands
-    class Ship
+  module T1k
+    module Commands
+      class Ship
 
-      def self.run
-        default_remote_branch = T1k::Repository.default_remote_branch
+        def self.run
+          default_remote_branch = T1k::Repository.default_remote_branch
 
-        branch = T1k::Commands::Branch.actual_branch.strip
+          branch = T1k::Commands::Branch.actual_branch.strip
 
-        if default_remote_branch == :master
-        [
-          "git checkout master",
-          "git pull --rebase",
-          "git merge #{branch}",
-          "git commit -v",
-          "git push origin master"
-        ].each do |cmd|
-           system cmd
-         end
+          if default_remote_branch == :master
+            [
+              "git checkout master",
+              "git pull --rebase",
+              "git merge #{branch}",
+              "git commit -v",
+              "git push origin master"
+            ].each do |cmd|
+               system cmd
+             end
 
-       elsif default_remote_branch == :branch
-         [
-           "git checkout #{branch}",
-           "git pull --rebase",
-           "git commit -v",
-           "git push origin #{branch}"
-         ].each do |cmd|
-            system cmd
-          end
+         elsif default_remote_branch == :branch
+             [
+               "git checkout #{branch}",
+               "git pull --rebase",
+               "git commit -v",
+               "git push origin #{branch}"
+             ].each do |cmd|
+                system cmd
+              end
+        end
       end
     end
   end
